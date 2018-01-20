30cb9d55358f93fb284c23dd89627caa72fa6be5
song <song1>
2018-01-21 01:41:32 +0800

    dao单元测试通过

---
+++ src/main/java/org/seckill/entity/SuccessKilled.java
@@ -0,0 +1,16 @@
+package org.seckill.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class SuccessKilled implements Serializable{
+    private long seckillId;
+    private long userPhone;
+    private short state;
+    private Date createTime;
+
+    private Seckill seckill;
+}
---
+++ src/test/java/org/seckill/dao/SeckillDaoTest.java
@@ -0,0 +1,43 @@
+package org.seckill.dao;
+
+import com.alibaba.fastjson.JSON;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.entity.Seckill;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:spring/applicationContext.xml")
+public class SeckillDaoTest {
+    @Resource
+    private SeckillDao seckillDao;
+
+    @Test
+    public void queryById() throws Exception {
+        long id=1000;
+        Seckill seckill = seckillDao.queryById(id);
+        System.out.println(JSON.toJSONString(seckill));
+    }
+
+    @Test
+    public void queryAll() throws Exception {
+        List<Seckill> seckillList = seckillDao.queryAll(0, 100);
+        System.out.println(JSON.toJSONString(seckillList));
+    }
+
+    @Test
+    public void reduceNumber() throws Exception {
+        Date keillTime = new Date();
+        int i = seckillDao.reduceNumber(1000L, keillTime);
+        System.out.println(i);
+    }
+
+}
